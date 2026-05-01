95ae5a1e6ae9e9a4b25e05c0bf313cef26ce538e
cekang <kecuni0405@gmail.com>
2026-05-01 15:07:02 +0900

    프로그래머스

---
+++ CodingTest/src/sub1/Solution.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/**
+ * 날짜 : 26/05/01
+ * 이름 : 강채은
+ * 내용 : 프로그래머스 기출문제 1번 문자출력*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
