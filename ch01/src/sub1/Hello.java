7c2e7f7fe7b0865c8fd47e8ee1c2be4a172174fb
cekang <kecuni0405@gmail.com>
2026-05-01 14:45:23 +0900

    add 1장 스터디

---
+++ ch01/src/sub1/Hello.java
@@ -0,0 +1,16 @@
+package sub1;
+
+/*
+ * 날짜 : 2026/04/27
+ * 이름 : 강채은
+ * 내용 : 1장 Java 개발환경 구축 실습
+ * */
+
+public class Hello {
+	
+	//모든 프로그래밍 언어의 상싱적인 입문 문자열
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Java");
+	}
+}
